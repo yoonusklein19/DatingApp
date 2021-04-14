124152894bed22c9f2448f5b7d0e91dc6019aee4
Yoonus <yoonus.kln@gmail.com>
2021-04-14 13:18:10 +0200

    End of Section 4

---
+++ API/Controllers/DTOs/LoginDTO.cs
@@ -0,0 +1,9 @@
+namespace API.Controllers.DTOs
+{
+    public class LoginDTO
+    {
+        public string Username { get; set;}
+
+        public string Password { get; set;}
+    }
+}
---
+++ API/Controllers/DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+namespace API.Controllers.DTOs
+{
+    public class UserDTO
+    {
+        public string Username { get; set; }
+
+        public string Token { get; set; }
+    }
+}
